c28ca9b6ac51c14313ae8297636d33ffd71204f5
Vijay Gupta <vijayg963@gmail.com>
2022-03-15 03:32:23 +0530

    Assignment-2

---
+++ block-BMaaah/query.js
@@ -0,0 +1,32 @@
+let books = [
+  {
+    title: 'book1',
+    details: 'on water',
+    author: {
+      name: 'horo',
+      email: 'p1@gmail.com',
+      age: '24',
+    },
+    tags: ['js', 'mongo'],
+  },
+  {
+    title: 'book2',
+    details: 'on Sea',
+    author: {
+      name: 'Jhon',
+      email: 'p2@gmail.com',
+      age: '25',
+    },
+    tags: ['js', 'mongo'],
+  },
+  {
+    title: 'book3',
+    details: 'on fire',
+    author: {
+      name: 'dav',
+      email: 'p3@gmail.com',
+      age: '22',
+    },
+    tags: ['js', 'mongo'],
+  },
+];
